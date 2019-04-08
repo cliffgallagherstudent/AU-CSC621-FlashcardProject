8c76d06f530dabbc2b355661150e0eec9c1d86d5
Cliff Gallagher <sgallagh@american.edu>
2019-04-08 06:47:40 -0400

    working on array

--- flashcardProgram.jl
+++ flashcardProgram.jl
@@ -25,7 +25,22 @@ while (newOpenOrQuit != "Quit")
 
         # if user wants to create a new deck of flashcards
         if (newOpenOrQuit == "New")
-            deckOne = DeckOfFlashcards
+            #=
+            println("What do you want to call this deck?")
+            deckName = readline()
+            deckOne = DeckOfFlashcards("deckName", Flashcard[])=#
+
+
+            #= Instead of having a DeckOfFlashcards object:
+            If I ask the user how many flashcards they want in the deck, I can create an array
+            of that many flashcard objects
+            Do I need a deckName? Or can I get away with just a filename? Maybe
+            something like "What do you want to call this deck? This will be the filename where
+            the individual flashcards are saved"
+            =#
+            println("How many flashcards do you want in this deck?")
+            numberOfCards = readline()
+            deckOne = Array{Flashcard, numberOfCards}
         end
 
         # if user wants to open an existing deck of flashcards
